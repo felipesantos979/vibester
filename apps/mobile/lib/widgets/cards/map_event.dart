e3a8f0c3066801b1054127d8b3905955f79e85b7
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2026-05-27 22:16:30 -0300

    SCRUM-76 fix: Corrigindo bug do maps no iphone
    corrigi o bug que acontecia ao clicar no botao de levar ao maps do local do evento no iphone.

---
+++ apps/mobile/lib/widgets/cards/map_event.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MapEvent extends StatefulWidget {
+  final String endereco;
+  const MapEvent({super.key, required this.endereco});
+
+  @override
+  State<MapEvent> createState() => _MapEventState();
+}
+
+class _MapEventState extends State<MapEvent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 0, vertical: 8),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        clipBehavior: Clip.antiAlias,
+        child: SizedBox(
+          height: 150,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.asset('assets/img/map_fundo.png', fit: BoxFit.cover),
+
+              Align(
+                alignment: Alignment.center,
+                child: GestureDetector(
+                  onTap: () async {
+                    String enderecoCodificado = Uri.encodeComponent(
+                      widget.endereco,
+                    );
+
+                    final Uri url = Uri.parse(
+                      "https://www.google.com/maps/search/?api=1&query=$enderecoCodificado",
+                    );
+
+                    await launchUrl(url, mode: LaunchMode.externalApplication);
+                  },
+
+                  child: Container(
+                    width: 250,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 12,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.map, color: Colors.black, size: 24),
+                        SizedBox(width: 8),
+                        Text(
+                          "ABRIR NO MAPS",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
