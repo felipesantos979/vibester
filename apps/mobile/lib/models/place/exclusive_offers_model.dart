de97e3031c3059f2c19959af39babf1f45c400ef
Ramon-Gabriel <marcosrrodriguescastro@gmail.com>
2026-05-29 23:07:20 -0300

    SCRUM-98 feat: seção de ofertas
    Implementada a seção de ofertas, seguindo o mesmo modelo da seção de FeaturedEvents que usava a implementação de um PageView para o carrosel dinamico

---
+++ apps/mobile/lib/models/place/exclusive_offers_model.dart
@@ -0,0 +1,13 @@
+class  ExclusiveOffersModel{
+  final String lugar;
+  final int desconto;
+  final String condicao;
+
+
+  ExclusiveOffersModel({
+    required this.lugar,
+    required this.desconto,
+    required this.condicao,
+
+  });
+}
