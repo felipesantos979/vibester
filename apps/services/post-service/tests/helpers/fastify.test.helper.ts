2568029300c8fe8e51fd991feb6324afb67cbbfa
GUINNAS <guimassucato017@gmail.com>
2026-06-28 05:42:23 -0300

    SCRUM-148 fix: Correções em erros de conexão do post-service com o redis e banco

--- apps/services/post-service/tests/helpers/fastify.test.helper.ts
+++ apps/services/post-service/tests/helpers/fastify.test.helper.ts
@@ -2,10 +2,7 @@ import Fastify from 'fastify';
 import { routes } from '../../src/routes';
 
 export async function buildServer() {
-  const app = Fastify({
-    logger: false,
-    ajv: { customOptions: { strict: false } },
-  });
+  const app = Fastify({ logger: false });
   await routes(app);
   return app;
 }
