77b7488d29edb56fc0b092e30662e572212c0b9e
Guilherme Massucato <guimassucato017@gmail.com>
2026-06-30 09:58:32 -0300

    SCRUM-175 fix: Correção no rollout do kubernetes para o feed-service

--- apps/services/feed-service/src/schema/events/post-liked.schema.ts
+++ apps/services/feed-service/src/schema/events/post-liked.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const postLikedSchema = z.object({
     postId: z.string().uuid(),
     userId: z.string().uuid(),
-    createdAt: z.string(),
+    createdAt: z.string().optional(),
 });
 
 export type PostLikedEvent = z.infer<typeof postLikedSchema>;
